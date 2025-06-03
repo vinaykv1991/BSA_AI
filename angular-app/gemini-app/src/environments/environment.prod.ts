5a65512b91a8dd7f0fc05781990280994e800bcb
Vinay <vinay.kv.91@gmail.com>
2025-06-03 10:11:16 +0530

    Update environment.prod.ts

--- angular-app/gemini-app/src/environments/environment.prod.ts
+++ angular-app/gemini-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'YOUR_RENDER_BACKEND_URL_HERE' // This will be the base URL of the deployed Render backend
+  apiUrl: 'https://bsa-ai.onrender.com' // This will be the base URL of the deployed Render backend
 };
